8c551050a862419dfb4e8d9fad0b08c216159d3d
Pournami Azad <pournami@qburst.com>
2017-10-26 11:15:06 +0530

    Added carousel indicators

--- js/main.js
+++ js/main.js
@@ -72,8 +72,8 @@ $(function(){
 	});
 
 	$('body').on('click', '.card', function() {
-		var activeElement = $(this);
-		var activeFilter = chooseCategory.val();
+		var activeElement = $(this),
+			activeFilter = chooseCategory.val();
 
 		if(activeFilter === "All") {
 			currentSlide = $(this).data('all-id');
@@ -87,7 +87,7 @@ $(function(){
 		showActiveElement(activeElement, activeFilter);
 		$('#showItem').modal('show');
 	});
-	
+
 	$('.btn-next').on('click', function(e){
 		e.preventDefault();
 		var activeFilter = chooseCategory.val(),
@@ -113,26 +113,39 @@ $(function(){
 	});
 
 	function showActiveElement(activeElement, activeFilter) {
-		var activeFilter = chooseCategory.val();
+		
 		if(activeFilter === "All") {
 			activeElement = $('.album-container').find("[data-all-id='"+currentSlide+"']");
 		}
 		else {
 			activeElement = $('.album-container').find("[data-index='"+currentSlide+"']");
 		}
+
 		var	imgSrc = activeElement.find('img').attr('src'),
 			imgTitle = activeElement.find('figcaption label').text(),
 			imgDesc = activeElement.find('figcaption .card-text').text(),
+			carouselIndicators = $('.carousel-indicators'),
 			selectedItemDesc;
 
 		selectedItemDesc = '<div class="container"><div class="row active"><div class="col selected-item-image"><img class="img-fluid" src=" '+ imgSrc +'" alt="Image"></div><div class="col selected-image-desc"><h3> '+ imgTitle +' </h3><p> '+ imgDesc +'</p></div></div></div>' ;
         $('.modal-body').html('').append(selectedItemDesc);
+
         $('.selected-item-image img').effect( "slide", 500 );
         $('.selected-image-desc h3').effect( "slide", 500 );
         $('.selected-image-desc p').animate({
         	left: 0,
         	opacity: '+=1'
         }, 500);
+
+        carouselIndicators.html('');
+		
+		carouselIndicators.each(function() {
+			for(i = 0; i < totalSlides; i++){
+				$("<li/>").appendTo(this);
+			}
+		});
+
+		carouselIndicators.find('li:nth-child('+ currentSlide +')').addClass('active');
 
     }
 
